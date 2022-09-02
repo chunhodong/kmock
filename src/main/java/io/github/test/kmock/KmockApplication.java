631269592ef6b6d4e04274ba446cb22a0585b33a
chunhodong <yhms4432@naver.com>
2022-09-02 22:35:47 +0900

    first commit

---
+++ src/main/java/io/github/test/kmock/KmockApplication.java
@@ -0,0 +1,13 @@
+package io.github.test.kmock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KmockApplication.class, args);
+	}
+
+}
