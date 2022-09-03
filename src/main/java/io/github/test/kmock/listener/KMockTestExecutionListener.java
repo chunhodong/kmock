8b24a3685b16c7235970c927021a8fb25a0a671b
chunhodong <yhms4432@naver.com>
2022-09-04 00:59:15 +0900

    [feat] 빈후처리기기능추가

--- src/main/java/io/github/test/kmock/listener/KMockTestExecutionListener.java
+++ src/main/java/io/github/test/kmock/listener/KMockTestExecutionListener.java
@@ -2,13 +2,8 @@ package io.github.test.kmock.listener;
 
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.env.Environment;
 import org.springframework.test.context.TestContext;
 import org.springframework.test.context.support.AbstractTestExecutionListener;
-
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Field;
-import java.util.Arrays;
 
 @Slf4j
 @ToString
--- src/main/java/io/github/test/kmock/postprocessor/KMockBeanFactoryPostProcessor.java
+++ src/main/java/io/github/test/kmock/postprocessor/KMockBeanFactoryPostProcessor.java
@@ -1,14 +1,35 @@
 package io.github.test.kmock.postprocessor;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.context.annotation.ScannedGenericBeanDefinition;
+
+import java.lang.reflect.Constructor;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class KMockBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
     @Override
     public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
-        //mock으로등록
+        //생성자 리스트조회
+        //생성자에서 객체추가
+        List<Constructor<?>> constructorList = Arrays.stream(beanFactory.getBeanDefinitionNames())
+                .map(beanDefinitionName -> beanFactory.getBeanDefinition(beanDefinitionName))
+                .filter(beanDefinition -> beanDefinition instanceof ScannedGenericBeanDefinition)
+                .map(beanDefinition -> {
+                    try {
+                        return BeanUtils.getResolvableConstructor(
+                                this.getClass().getClassLoader().loadClass(beanDefinition.getBeanClassName()));
+                    } catch (ClassNotFoundException e) {
+                        return null;
+                    }
+                })
+                .collect(Collectors.toList());
     }
 }
